a510638adb8d8f85fddeb8d1677f5e1a0ee91a07
2200090246 <2200090246@kluniversity.in>
2024-04-29 15:27:24 +0530

    my first commit

---
+++ db/config.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://2200090246:ms231435@cluster0.kypqrfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/event-management");
